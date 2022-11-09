92a0817663377d9a106d0f268282ac2120217279
Juan Fernando Martinez Dominguez <jm1811324@gmail.com>
2022-11-09 17:56:06 -0500

    change size for mobile

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ class AppComponent extends React.Component {
   }
   render(){
     return (
-      <div className = "m-auto w-50 p-5 bg-light rounded border shadow-lg mt-5 bg-black" >
+      <div className = "m-auto w-75 p-5 bg-light rounded border shadow-lg mt-5 bg-black" >
       <head>
       
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous"/>
